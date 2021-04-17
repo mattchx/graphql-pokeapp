28bc6d23b98e8c8e4ea9feaa95749c727e51b74d
Matt <mattcharlesh@gmail.com>
2021-04-16 20:58:44 -0400

    formatted PokemonContainer

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import {Pokemon} from './components/Pokemon'
+import { Pokemon } from '../components/Pokemon';
+import { GET_POKEMONS } from '../graphql/get-pokemons'
 
 export function PokemonContainer() {
-  const { 
-      data: { pokemons = [] } = {}
-  } = useQuery(GET_POKEMONS, {variables: {first: 9},
-})
-
-  return <div className="container">
-      {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} /> )}
-      </div>;
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+  console.log(pokemons)
+  return (
+    <div className="container">
+      {pokemons &&
+        pokemons.map((pokemon) => (
+          <Pokemon key={pokemon.id} pokemon={pokemon} />
+        ))}
+    </div>
+  );
 }
