b73f56a89aa62690b41592643503c1762765c4d6
Matt <mattcharlesh@gmail.com>
2021-04-16 20:08:48 -0400

    Create PokemonContainer.js

---
+++ src/containers/PokemonContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+export function PokemonContainer() {
+    return (
+        <p>
+            placeholder
+        </p>
+    )
+}
